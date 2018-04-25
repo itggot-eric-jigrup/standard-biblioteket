e05fa82819b1acd26946329738789bf2d558548b
itggot-eric-jigrup <eric.jigrup@itggot.se>
2018-04-25 13:49:20 +0200

    added documentation to strictbetween

--- lib/strictbetween.rb
+++ lib/strictbetween.rb
@@ -1,3 +1,13 @@
+# num1  - The Integer compared.
+# num2  - The Integer to compare.
+# num3  - the Integer to compare.
+#
+# Examples
+#
+#   between(5,1,10)
+#   # => "true"
+#
+# Returns the corresponding string.
 def strictbetween(num1, num2, num3)
     i = "false"
     if num1 > num2
