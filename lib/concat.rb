fcfce97468eb6924de9d09d06c0eb84946c92d5f
itggot-eric-jigrup <eric.jigrup@itggot.se>
2018-04-25 14:09:33 +0200

    added documentation to concat

--- lib/concat.rb
+++ lib/concat.rb
@@ -1,3 +1,12 @@
+# arr1  - The Array to use.
+# arr2  - The Array to push in.
+#
+# Examples
+#
+#   minoftwo([1,3],[4,2])
+#   # => [1,3,4,2]
+#
+# Returns the Array.
 def concat(arr1, arr2)
     return arr1 << arr2 
 end
