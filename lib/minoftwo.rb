03199264c6e16fa27514ddc6016a924259e01d29
itggot-eric-jigrup <eric.jigrup@itggot.se>
2018-04-25 13:50:58 +0200

    added documentation to minoftwo

--- lib/minoftwo.rb
+++ lib/minoftwo.rb
@@ -1,3 +1,12 @@
+# num1  - The Integer to compare.
+# num2  - The Integer to compare.
+#
+# Examples
+#
+#   minoftwo(20,100)
+#   # => 20
+#
+# Returns the smallest Integer.
 def minoftwo(num1, num2)
     if num1 > num2
         i = num2
