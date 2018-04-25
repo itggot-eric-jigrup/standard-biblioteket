636c4d3fca73b5161d854334d36161418c99a2d7
itggot-eric-jigrup <eric.jigrup@itggot.se>
2018-04-25 14:10:59 +0200

    added documentation to prepend

--- lib/prepend.rb
+++ lib/prepend.rb
@@ -1,3 +1,12 @@
+# num  - The Integer to use.
+# arr  - The Array to push in.
+#
+# Examples
+#
+#   minoftwo(4,[3,2])
+#   # => [4,3,2]
+#
+# Returns the Array.
 def prepend(num, arr)
     arr2 = []
     arr2 << num 
